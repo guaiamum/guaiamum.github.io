7b6392f213a3e8e9b5a58de701c3df59bcef5b04
Cainã Brazil <CainaBrazil@gmail.com>
2020-04-21 20:59:05 +0100

    first

---
+++ src/components/App/__tests__/app.test.js
@@ -0,0 +1,27 @@
+import { h } from 'preact';
+import { mount } from 'enzyme';
+
+import App from '../index';
+
+describe('App', () => {
+    it('should surprise me', () => {
+        jest.mock('Components/Icon', () => ({}));
+        const wrapper = mount(<App />);
+        expect(wrapper.text()).toInclude('Current value: 5');
+    });
+});
+
+// describe('Counter', () => {
+//   it('should display initial count', () => {
+//     const wrapper = mount(<Counter initialCount={5}/>);
+//     expect(wrapper.text()).to.include('Current value: 5');
+//   });
+
+//   it('should increment after "Increment" button is clicked', () => {
+//     const wrapper = mount(<Counter initialCount={5}/>);
+
+//     wrapper.find('button').simulate('click');
+
+//     expect(wrapper.text()).to.include('Current value: 6');
+//   });
+// });
